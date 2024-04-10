2385513a36db555c752300030ff27c321d92d724
Adriano <rodrigues.adriano2607@gmail.com>
2024-04-10 20:19:54 -0300

    adicionadoDrawer

--- src/componentes/Body.tsx
+++ src/componentes/Body.tsx
@@ -12,6 +12,20 @@ import "dayjs/locale/pt-br";
 import { useNavigate } from "react-router-dom";
 import { GamesProps } from "../interface/interfaceGame";
 import { TitulosH1 } from "./Titulos";
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+
+
+
 
 dayjs.extend(relativeTime);
 dayjs.locale("pt-br");
@@ -85,6 +99,7 @@ export const Body = () => {
       categoryValue = "android";
     }
     setSelectedCategory(categoryValue);
+    setOpen(false)
   }
 
   function whatsFilter() {
@@ -119,9 +134,43 @@ export const Body = () => {
     fetchData(url);
   }
 
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = (newOpen: boolean) => () => {
+    setOpen(newOpen);
+  };
+
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+    
+    </Box>
+  );
+
+  
   return (
+   
+  
+    
     <div className="flex flex-wrap gap-7 justify-center ">
+        <Button onClick={toggleDrawer(true)}>Open drawer</Button>
+      <Drawer open={open} onClose={toggleDrawer(false)}>
+          {DrawerList}
+      </Drawer>
       <header className="bg-zinc-800 w-full pb-5 flex justify-center border-b border-white/30 flex-col ">
+  
         <div className="flex justify-between items-center">
           <div>
             <select
